54f59f2f4f65d8fc0a009fa605f7b636356ad57e
xulawyer1982 <56947483+xulawyer1982@users.noreply.github.com>
2026-07-08 14:10:42 +0800

    Update rc.local download URL in diy-part2.sh

--- diy/diy-part2.sh
+++ diy/diy-part2.sh
@@ -11,7 +11,7 @@
 # DIY编译⬇⬇⬇
 # ================================================================
 # 集成config
-wget -qO- https://raw.githubusercontent.com/Kwonelee/iStoreOS-Actions/refs/heads/main/files/etc/rc.local > package/base-files/files/etc/rc.local
+wget -qO- https://raw.githubusercontent.com/xulawyer1982/iStoreOS-Actionsx/refs/heads/main/files/etc/rc.local > package/base-files/files/etc/rc.local
 
 # 移除要替换的包
 rm -rf feeds/packages/net/adguardhome
